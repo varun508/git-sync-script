6b3bca314e2c8e954fa883cf9e9a0b8fa08deb56
Varun Sharma <imva508@gmail.com>
2019-02-27 22:24:01 +0530

    can now add commit message with quotes

--- gitc.cmd
+++ gitc.cmd
@@ -1,10 +1,7 @@
-set commitmsg=%1
-echo Adding files...
+set commitmsg=%~1%
 git add -A
 echo Done adding files
-echo Commiting %commitmsg%
 git commit -a -m %commitmsg%
 echo Done
-echo Pushing files to master...
 git push origin master
 echo Done
