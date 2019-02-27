3ac94101d02c1bc7c226b37b7aa2963f64c3d277
Varun Sharma <imva508@gmail.com>
2019-02-27 17:55:16 +0530

    Update gitc.cmd

--- gitc.cmd
+++ gitc.cmd
@@ -1,5 +1,10 @@
-echo off
 set commitmsg=%1
+echo Adding files...
 git add -A
+echo Done adding files
+echo Commiting %commitmsg%
 git commit -a -m %commitmsg%
+echo Done
+echo Pushing files to master...
 git push origin master
+echo Done
